b03141ae64fc02c30f89a82690d1e57a2c6300c1
mohammedf16X <fivehjoftoj366@gmail.com>
2025-07-17 04:57:44 +0300

    Update Darkspawner1.lua

--- Darkspawner1.lua
+++ Darkspawner1.lua
@@ -1,3 +1,5 @@
 local Spawner = loadstring(game:HttpGet("https://codeberg.org/GrowAFilipino/GrowAGarden/raw/branch/main/Spawner.lua"))()
 Spawner.Load()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/mohammedf16X/DARKSPAWNER/refs/heads/main/Darkspawner.lua"))()
+wait(1)
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/mohammedf16X/DARKSPAWNER/refs/heads/main/Darkspawner.lua"))()
+Notification.Load()
